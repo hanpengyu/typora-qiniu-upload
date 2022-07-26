85f398574c76c1c996d80af05d34604b7124c992
hanpy <hanpengyu521@126.com>
2022-07-26 19:29:33 +0800

    优化配置获取逻辑

---
+++ config/config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"fmt"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+var conf *Config
+
+type Config struct {
+	cfg *viper.Viper
+}
+
+func GetConfig() *viper.Viper {
+	return conf.cfg
+}
+
+func setConfig(c *Config) {
+	conf = c
+}
+
+func InitConfig(f *pflag.FlagSet) {
+	rootViper := viper.New()
+	err := rootViper.BindPFlags(f)
+	if err != nil {
+		panic("配置文件绑定错误")
+	}
+
+	// 配置文件路径
+	configPath := rootViper.GetString("config")
+	if configPath == "" {
+		configPath = "/usr/local/etc/qiniu.toml"
+	}
+
+	// 解析配置文件
+	qnViper := viper.New()
+	qnViper.SetConfigFile(configPath)
+	err = qnViper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Sprintf("配置文件解析失败:%s", configPath))
+	}
+
+	conf := &Config{
+		cfg: qnViper,
+	}
+	setConfig(conf)
+}
