63565b1d9d9a0189f43e56007cd8c72a3c838b58
hanpy <hanpengyu521@126.com>
2022-07-27 15:39:56 +0800

    调整结构

--- cmd/upload.go
+++ cmd/upload.go
@@ -3,7 +3,7 @@ package cmd
 import (
 	"fmt"
 	"github.com/spf13/cobra"
-	"typora-qiniu-upload/core"
+	"typora-qiniu-upload/utils"
 )
 
 var (
@@ -20,11 +20,26 @@ func init() {
 	rootCmd.AddCommand(uploadCmd)
 }
 
+/**
+ *  @Description: 上传图片
+ *  @Author: HanPengYu
+ *  @param args
+ **/
 func upload(args []string) {
-	rstStr, err := core.UploadImg(args)
-	if err != nil {
-		fmt.Println(err.Error())
+	var rstStr string = "Upload Faild:"
+
+	var succFiles = make([]string, 0)
+	for _, fileInfo := range args {
+		imageUrl, err := utils.UploadImageTuQiNiuByForm(fileInfo)
+		if err == nil {
+			succFiles = append(succFiles, imageUrl)
+		}
+	}
+
+	if len(succFiles) == 0 {
+		fmt.Println(utils.ErrorString(rstStr, "文件全部上传失败"))
 		return
 	}
-	fmt.Println(rstStr)
+
+	fmt.Println(utils.SuccessString("Upload Success:", succFiles))
 }
